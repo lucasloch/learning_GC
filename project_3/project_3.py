1debcc954c72eb70cd2f9535933b0159d4705692
Lucas Loch <lucas.loch.79@gmail.com>
2022-09-15 18:22:16 -0300

    Update project_3.py

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -36,13 +36,13 @@ casa_escalonada = casa * matriz_escala
 plt.plot(casa_escalonada[:,0], casa_escalonada[:,1], color='orange', label='Escala')
 
 # Reflexão em relação ao eixo X
-matriz_translacao = np.matrix([
+matriz_reflexao = np.matrix([
     [1, 0, 0], 
     [0, -1, 0], 
     [0, 0, 1]
 ])
-casa_transladada = casa * matriz_translacao
-plt.plot(casa_transladada[:,0], casa_transladada[:,1], color='purple', label='Reflexão')
+casa_refletida = casa * matriz_reflexao
+plt.plot(casa_refletida[:,0], casa_refletida[:,1], color='purple', label='Reflexão')
 
 # Rotação
 matriz_rotacao = np.matrix([
@@ -54,13 +54,13 @@ casa_rotacionada = casa * matriz_rotacao
 plt.plot(casa_rotacionada[:,0], casa_rotacionada[:,1], color='green', label='Rotação')
 
 # Cisalhamento
-matriz_translacao = np.matrix([
+matriz_cisalhamento = np.matrix([
     [1, 0, 0], 
     [1, 1, 0], 
     [0, 0, 1]
 ])
-casa_transladada = casa * matriz_translacao
-plt.plot(casa_transladada[:,0], casa_transladada[:,1], color='black', label='Cisalhamento')
+casa_cisalhada = casa * matriz_cisalhamento
+plt.plot(casa_cisalhada[:,0], casa_cisalhada[:,1], color='black', label='Cisalhamento')
 
 # Normal
 plt.plot(casa[:,0], casa[:,1], color='blue', label='Normal')
